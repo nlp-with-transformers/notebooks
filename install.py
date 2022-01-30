fa1c219ec3db23e48c0849be2ef27665efa4d5a8
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2022-01-30 11:03:41 +0100

    Refactor install

--- install.py
+++ install.py
@@ -3,24 +3,33 @@ import sys
 
 is_colab = "google.colab" in sys.modules
 is_kaggle = "kaggle_secrets" in sys.modules
-torch2cuda = {"1.10.0": "cu113", "1.9.0": "cu111", "1.9.1": "cu111"}
+# torch-scatter binaries depend on the torch and CUDA version, so we define the
+# mappings here for Colab & Kaggle
+scatter_matrix = {"1.10.0": "cu113", "1.9.0": "cu111", "1.9.1": "cu111"}
 
 
-def install_requirements(is_qa: bool = False):
+def install_requirements(is_chapter7: bool = False, is_chapter11: bool = False):
     """Installs the required packages for the project."""
+
     print("Installing base requirements ...")
-    if is_qa:
-        requirements = "requirements_qa.txt -f https://download.pytorch.org/whl/torch_stable.html".split()
+    if is_chapter7:
+        requirements = "requirements-chapter7.txt -f https://download.pytorch.org/whl/torch_stable.html".split()
     else:
         requirements = "requirements.txt"
     process_install = subprocess.run(
         ["python", "-m", "pip", "install", "-r", requirements], stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     if process_install.returncode != 0:
-        raise Exception("Failed to install requirements")
+        raise Exception("Failed to install base requirements")
     else:
         print("Base requirements installed!")
-    if is_colab or is_kaggle:
+    print("Installing Git LFS ...")
+    process_lfs = subprocess.run(["apt", "install", "git-lfs"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process_lfs.returncode == -1:
+        raise Exception("Failed to install Git LFS and soundfile")
+    else:
+        print("Git LFS installed!")
+    if is_chapter11:
         import torch
 
         torch_version = torch.__version__.split("+")[0]
@@ -34,7 +43,7 @@ def install_requirements(is_qa: bool = False):
                 "install",
                 "torch-scatter",
                 "-f",
-                f"https://data.pyg.org/whl/torch-{torch_version}+{torch2cuda[torch_version]}.html",
+                f"https://data.pyg.org/whl/torch-{torch_version}+{scatter_matrix[torch_version]}.html",
             ],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -43,12 +52,12 @@ def install_requirements(is_qa: bool = False):
             raise Exception("Failed to install torch-scatter")
         else:
             print("torch-scatter installed!")
-        print("Installing Git LFS and soundfile ...")
-        process_lfs = subprocess.run(
-            ["apt", "install", "git-lfs", "libsndfile1"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        print("Installing soundfile ...")
+        process_audio = subprocess.run(
+            ["apt", "install", "libsndfile1"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
-        if process_lfs.returncode == -1:
-            raise Exception("Failed to install Git LFS and soundfile")
+        if process_audio.returncode == -1:
+            raise Exception("Failed to install soundfile")
         else:
-            print("Git LFS and soundfile installed!")
+            print("soundfile installed!")
         print("Install complete!")
